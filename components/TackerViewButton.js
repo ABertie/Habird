f965d2a814a9738ee4f7e7e7b345cf74050c368c
ABertie <alberte.reuss@gmail.com>
2024-08-07 16:48:23 +0200

    save

---
+++ components/TackerViewButton.js
@@ -0,0 +1,26 @@
+import { Pressable, StyleSheet, Text } from "react-native";
+import { Dark, Mid } from "./colors";
+
+export default function TackerViewButton({ label, onPress, selected }) {
+    return (
+        <Pressable style={selected ?
+            [styles.button, {
+                backgroundColor: Mid,
+            }]
+            : styles.button} onPress={onPress}>
+            <Text style={styles.text}>{label}</Text>
+        </Pressable>
+    )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        flex: 1,
+        borderRadius: 8,
+        padding: 8,
+    },
+    text: {
+        textAlign: 'center',
+        color: Dark,
+    }
+})
---
+++ components/colors.js
@@ -0,0 +1,6 @@
+export const Brand = "#F5BD86"
+export const Darkest = "#212325"
+export const Dark = "#484C52"
+export const Mid = "#E6CDB4"
+export const Light = "#F1EBE5"
+export const Lightest = "#FFFFFF"
